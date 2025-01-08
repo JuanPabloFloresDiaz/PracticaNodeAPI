7e88c5d4c646b8742e11842acbb3dc318614f298
Juan Pablo Flores Díaz <101837425+JuanPabloFloresDiaz@users.noreply.github.com>
2025-01-08 15:52:49 -0600

    Corrección con el metodo de verificación de la jwt

--- src/middleware/jwt.js
+++ src/middleware/jwt.js
@@ -12,19 +12,38 @@ const generateJWT = (userData) => {
 
 // Middleware para verificar un JWT en las solicitudes de Express
 const verifyJWT = (req, res, next) => {
-  const token = req.headers['authorization']; // Leer el token del encabezado
+  const authHeader = req.headers['authorization']; // Leer el encabezado Authorization
+  if (!authHeader) {
+    return res.status(403).json({
+      status: 0,
+      message: 'Token no proporcionado',
+      dataset: null,
+      error: 'El token no fue proporcionado',
+      exception: null,
+    });
+  }
+
+  // Extraer el token sin el prefijo 'Bearer '
+  const token = authHeader.split(' ')[1];
   if (!token) {
-    return res.status(403).json({ status: 0, message: 'Token no proporcionado', dataset: null, error: 'El token no fue proporcionado', exception: null });
+    return res.status(403).json({
+      status: 0,
+      message: 'Token no proporcionado o malformado',
+      dataset: null,
+      error: 'El token no fue proporcionado correctamente',
+      exception: null,
+    });
   }
 
   try {
-    const decoded = jwt.verify(token, secretKey); // Verificar token
+    const decoded = jwt.verify(token, secretKey); // Verificar token puro
     req.user = decoded; // Agregar el usuario decodificado a la solicitud
     next();
   } catch (error) {
     return res.status(401).json({ message: 'Token inválido o expirado' });
   }
 };
+
 
 // Función para validar un JWT directamente (opcional)
 const validateJWT = (token) => {
